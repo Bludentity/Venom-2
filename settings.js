b0b3569e7bb7a38d2b1cbb7b55e160727be84ef3
Bludentity <bludentity@gmail.com>
2024-07-22 08:18:01 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -4,14 +4,14 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "916909137213"
-global.ownername = "David Cyril"
+global.ownernomer = "254702351657"
+global.ownername = "Alviin21💀💀"
 global.ytname = "David Cyril"
 global.socialm = "GitHub: DeeCeeXxx"
 global.location = "Africa, Nigeria, Lagos"
 
-global.ownernumber = '2347043759577,2349066528353,254782572110'  //creator number
-global.ownername = 'David Cyril' //owner name
+global.ownernumber = '254702351657'  //creator number
+global.ownername = 'Alviin21💀💀' //owner name
 global.botname = '🕷Venom-2🕷' //name of the bot
 
 //sticker details
